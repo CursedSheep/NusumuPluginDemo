d7f2dcd8971a05c273e271d1de734d50ebe8d3f5
CursedSheep <44673993+CursedSheep@users.noreply.github.com>
2020-12-08 01:51:02 +0800

    Add files via upload

---
+++ NusumuPlugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NusumuPlugintest
+{
+    public class NusumuPlugin
+    {
+        //DO NOT ADD PARAMETERS!
+        //Plugin will not work with parameters ex: Execute(object obj1, object obj2)
+        public static void Execute()
+        {
+            MessageBox.Show("test");
+        }
+    }
+}
